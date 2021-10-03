a6b95196a13e378418f43e12aaeade0bf0cf97c3
patriklussi <46919194+patriklussi@users.noreply.github.com>
2021-10-03 17:05:48 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,17 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <SearchFilter msg="Welcome to Your Vue.js App"/>¨
-
   </div>
 </template>
 
 <script>
 import SearchFilter from "./components/SearchFilter.vue"
 
-
 export default {
   name: 'App',
   components: {
     SearchFilter,
- 
   },
-  async mounted(){
-    const response = await fetch("http://ddragon.leagueoflegends.com/cdn/11.19.1/data/en_US/champion.json");
-    const data = await response.json();
-    console.log(data);
-  }
 }
 </script>
 
