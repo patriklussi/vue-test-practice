20111727247dd09492d3c1906410b16763ba2b6e
patriklussi <46919194+patriklussi@users.noreply.github.com>
2021-10-03 17:07:44 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,28 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <SearchFilter msg="Welcome to Your Vue.js App"/>¨
+    <FruitSalad/>
   </div>
 </template>
 
 <script>
 import SearchFilter from "./components/SearchFilter.vue"
+import FruitSalad from "./components/FruitSalad.vue"
 
 export default {
   name: 'App',
   components: {
     SearchFilter,
+    FruitSalad
   },
+  data:()=> {
+      return {
+        array :[
+
+        ]
+      }
+  },
+ 
 }
 </script>
 
@@ -24,5 +35,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  background:red;
 }
 </style>
